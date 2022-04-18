fdd35bdd877e322ac2ed7ce27b9274db6ceb2124
Ana <an.gordievich@gmail.com>
2022-04-18 19:34:50 +0300

    upd

--- beauty_shop.py
+++ beauty_shop.py
@@ -17,8 +17,8 @@ class Product:
     img_link: str
     link_to_product: str
     img_path: str
-    id: int
 
+    id: int = 0
     volume: str = ''
     origin: str = ''
     gender: str = ''
@@ -37,6 +37,11 @@ class ProductList:
         pass
 
 
+# def create_b_soup(url: str)->BeautifulSoup:
+#     res = requests.get(url)
+#     bs = BeautifulSoup(res.text, 'lxml')
+
+
 def pars_main(url: str) -> list:
     res = requests.get(url)
     bs = BeautifulSoup(res.text, 'lxml')
@@ -47,21 +52,32 @@ def pars_main(url: str) -> list:
         link = BASE_URL + card.find('a').attrs['href']
         name = card.find('div', class_='card-inner').text.strip()
         category = card.find('div', class_='card-category').text.strip().strip('#')
-        price = int(re.sub("[^0-9]", "", card.find('div', class_='card-price').text.strip()))
-        img_link = BASE_URL + card.find('div', class_='card-image').find('a').attrs['href']
+        try:
+            price = int(re.sub("[^0-9]", "", card.find('div', class_='card-price').text.strip()))
+        except:
+            price = None
+            print(link)
+        img_link = BASE_URL + card.find('div', class_='card-image').find('img').attrs['src']
         brand = card.find('div', class_='card-brand').text.strip()
-        img_path = bs.find('div', class_='card-image').find('img').attrs['src']
-        id = bs.find('div', class_='card-footer').find('button').attrs['data-id']
-        product_list.append(Product(name, category, price, brand, img_link, link, img_path, id))
-    print(product_list)
-    print(url)
+        img_path = img_link.split('/')[-1]
+        # id = bs.find('div', class_='card-footer').find('button').attrs['data-id']
+        product_list.append(Product(name, category, price, brand, img_link, link, img_path))
+    # print(product_list)
+    # print(url)
     return product_list
 
 
 def pars_extra(product: Product):
     res = requests.get(product.link_to_product)
     bs = BeautifulSoup(res.text, 'lxml')
+
     table = bs.find_all('tr')
+    try:
+        # product.id = bs.find('meta', itemprop='mpn').attrs['content']
+        product.id = bs.find('meta', itemprop='mpn').attrs['content']
+    except:
+        product.id = None
+        print(f'{product.link_to_product} нет id')
     for elem in table:
         if elem.find('th').text == 'Об\'єм':
             product.volume = elem.find('td').text.strip()
@@ -90,7 +106,7 @@ def grabber(links: list, parser):
             threads.append(executor.submit(parser, page))
         for page in as_completed(threads):
             elements += page.result()
-    print(elements)
+    # print(elements)
     return elements
 
 
@@ -98,7 +114,8 @@ def collect_main_data():
     base_category_url = 'https://sisters.co.ua/kosmetyka-dlya-volossa/'
     pages_in_cat = pages_in_category(base_category_url)
     links_list = []
-    for page in range(1, pages_in_cat + 1):
+    for page in range(90, 91):
+    # for page in range(1, pages_in_cat + 1):
         links_list.append(base_category_url + 'page/' + str(page))
     products = grabber(links_list, pars_main)
     full_products = grabber(products, pars_extra)
@@ -109,14 +126,14 @@ def to_json(data: list):
     general_list = []
     for item in data:
         general_list.append(item.__dict__)
-    with open('products.json', 'w') as file:
-        json.dump(general_list, file, indent=4)
+    with open('products_test.json', 'w') as file:
+        json.dump(general_list, file, indent=4, ensure_ascii=False)
 
 
 list_of_shamp = collect_main_data()
-# collect_main_data()
-print('-----------')
-for item in list_of_shamp:
-    print(item)
-print(len(list_of_shamp))
+collect_main_data()
+# print('-----------')
+# for item in list_of_shamp:
+#     print(item)
+# print(len(list_of_shamp))
 to_json(list_of_shamp)
