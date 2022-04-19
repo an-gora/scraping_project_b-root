22ff905e19909919755d1e9a8e39b412dc27c275
Ana <an.gordievich@gmail.com>
2022-04-19 21:29:01 +0300

    upd

--- beauty_shop_oop.py
+++ beauty_shop_oop.py
@@ -82,7 +82,6 @@ class ProductList:
     def pars_extra(self, product: Product):
         res = requests.get(product.link_to_product)
         bs = BeautifulSoup(res.text, 'lxml')
-
         table = bs.find_all('tr')
         try:
             product.id = bs.find('meta', itemprop='mpn').attrs['content']
@@ -124,7 +123,7 @@ class ProductList:
         pages_in_cat = self.pages_in_category(base_category_url)
         links_list = []
         for page in range(90, 91):
-            # for page in range(1, pages_in_cat + 1):
+            #for page in range(1, pages_in_cat + 1):
             links_list.append(base_category_url + 'page/' + str(page))
         products = self.grabber(links_list, self.pars_main)
         full_products = self.grabber(products, self.pars_extra)
@@ -137,7 +136,8 @@ class ProductList:
         with open('products.json', 'w') as file:
             json.dump(general_list, file, indent=4, ensure_ascii=False)
 
-    def save_image(self, product: Product):
+    @staticmethod
+    def save_image(product: Product):
         path_to_img_dir = path.join(path.dirname(__file__), 'img_dir')
         if not os.path.exists(path_to_img_dir):
             os.mkdir(path_to_img_dir)
@@ -146,13 +146,13 @@ class ProductList:
             photo.write(requests.get(product.img_link).content)
 
 
-pl = ProductList()
-pl.data_from_web()
-# pl.to_json()
+# pl = ProductList()
+# pl.data_from_web()
 # pl.write_to_file()
-# pl_2 = ProductList.load_from_file()
-# pl_2.to_json()
+# pl.to_json()
+pl_2 = ProductList.load_from_file()
+pl_2.to_json()
 # pl_2.save_image()
-# for product in pl_2:
-#     print(product.name)
-#     pl_2.save_image(product)
+for product in pl_2:
+    print(product.name)
+    pl_2.save_image(product)
